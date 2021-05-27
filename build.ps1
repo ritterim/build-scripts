d86b5aef26bba1c327a13f44f83dd850236e0a36
Thomas Harold <tgharold@gmail.com>
2021-05-27 11:33:24 -0400

    Add some debug output for each tested script
    also pass -Verbose and --verbosity=Normal to the bootstrap script

--- build.ps1
+++ build.ps1
@@ -6,7 +6,9 @@ $scripts = @(".\build-webapi-netfx.cake")
 # Verify $scripts do not contain compilation errors
 foreach ($script in $scripts) {
   Write-Output ""
+  Write-Output "----- TESTING: $script -----"
   .\bootstrap-cake.ps1 -Script $script -Verbose --verbosity=Normal --Target=Clean --exclusive
+  Write-Output "----- FINISH: $script -----"
 
   if ($LastExitCode -ne 0) {
       $failed = $true
